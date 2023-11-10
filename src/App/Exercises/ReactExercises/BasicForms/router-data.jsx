4b989dcfe5634f3414327b980cc9bb9c283e644f
Vladyslav Zhorov <vlad.zhorov@gmail.com>
2023-11-10 20:41:48 +0100

    Basic, Order Form

---
+++ src/App/Exercises/ReactExercises/BasicForms/router-data.jsx
@@ -0,0 +1,10 @@
+import { BasicForms } from './index';
+
+export const basicFormsMetaData = {
+  path: 'basic-fomrs',
+  date: null,
+  linkLabel: 'Elementy podstawowe formularzy',
+  blockNo: 42,
+  element: <BasicForms />,
+  tags: ['forms', 'form', 'formularz', 'input', 'checkbox', 'textarea'],
+};
